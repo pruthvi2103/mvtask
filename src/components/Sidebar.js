ab691d7b55b035dbe85167344ff7fb50d1fc2fa9
Pruthvi <pruthvishetty5656@gmail.com>
2020-11-02 03:11:34 +0530

    Sidebar Icons Added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,52 +1,94 @@
-import React from 'react'
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
+import TickIcon from "../assets/tickdone.svg";
+import DownArrow from "../assets/down-arrow.svg";
 const SidebarWrapper = styled.div`
-padding:24px;
+  padding: 24px 12px;
 `;
 const SidebarTitle = styled.h1`
-font-weight:600;
+  font-weight: 600;
 `;
-const SidebarSubtitle = styled.span`
-`;
+const SidebarSubtitle = styled.span``;
 const SidebarSubHeading = styled.span`
-font-weight:550;
-display:block;
-padding: 8px 0;
+  font-weight: 550;
+  display: block;
+  padding: 8px 0;
 `;
 const ListItem = styled.span`
-display:block;
-padding:12px 4px;
+  display: block;
+  margin: 12px 0;
+  padding: 12px 10px;
+  background: ${(props) => {
+    return props.active ? "rgba(255, 255, 255, 0.28)" : null;
+  }};
+  border-radius: 6px;
 `;
 const DropDown = styled.span`
-display:block;
-font-weight:550;
-padding: 45px 5px;
+  display: block;
+  font-size: 16px;
+  font-weight: 600;
+  padding: 45px 5px;
+`;
+const TickBackground = styled.div`
+  width: 27px;
+  height: 27px;
+  border-radius: 50%;
+  background: #fff;
+  display: flex;
+  align-items: center;
+`;
+const Icon = styled.img`
+  height: 13px;
+  width: 13px;
+  margin: auto;
+`;
+const PostFixItem = styled.div`
+  float: right;
+`;
+const Steps = styled.div`
+  float: right;
+  font-size: 14px;
 `;
 export const Sidebar = () => {
-    const listItems = [{name:"Add Users"},{name:"Install Software"},{name:"Product Walkthrough"}];
-
-    const renderListItems = () => {
-        return(
-            listItems.map(
-                (item,index) => {
-                   return (
-                       <ListItem key={index}>
-                           {item.name}
-                       </ListItem>
-                   )
-                }
-            )
-        )
-    }
+  const CompleteBadge = () => {
     return (
-        <SidebarWrapper>
-        <SidebarTitle>Hello, Bruce</SidebarTitle>
-        <SidebarSubHeading>Getting Started!</SidebarSubHeading> 
-        <SidebarSubtitle>Great to have you onboard!
-Lorem ipsum dolor sit amet</SidebarSubtitle>
-<DropDown> ONBOARDING SETUP </DropDown>
-        {renderListItems()}
-        </SidebarWrapper>
-    )
-}
+      <TickBackground>
+        <Icon src={TickIcon} />
+      </TickBackground>
+    );
+  };
+  const StepIndicator = () => {
+    return <Steps>1 of 2</Steps>;
+  };
+  const listItems = [
+    { name: "Add Users", postfix: <CompleteBadge /> },
+    { name: "Install Software", postfix: <StepIndicator />, active: true },
+    { name: "Product Walkthrough" },
+  ];
+
+  const renderListItems = () => {
+    return listItems.map((item, index) => {
+      return (
+        <ListItem active={item.active} key={index}>
+          {item.name}
+          {item.postfix && <PostFixItem>{item.postfix}</PostFixItem>}
+        </ListItem>
+      );
+    });
+  };
+  return (
+    <SidebarWrapper>
+      <SidebarTitle>Hello, Bruce</SidebarTitle>
+      <SidebarSubHeading>Getting Started!</SidebarSubHeading>
+      <SidebarSubtitle>
+        Great to have you onboard! Lorem ipsum dolor sit amet
+      </SidebarSubtitle>
+      <DropDown>
+        ONBOARDING SETUP{" "}
+        <Icon style={{ float: "right" }} src={DownArrow}></Icon>
+      </DropDown>
+
+      {renderListItems()}
+    </SidebarWrapper>
+  );
+};
